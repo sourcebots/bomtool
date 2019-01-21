5c9cfd75441abed574c4fb618870362bf798e87f
Kier Davis <kierdavis@gmail.com>
2019-01-21 13:41:15 +0000

    Add hit/miss logging for cache

--- bomtool/util/cache.py
+++ bomtool/util/cache.py
@@ -1,20 +1,25 @@
 import datetime
 import hashlib
 import pathlib
+import logging
 
 class Cache(object):
   def __init__(self, path=None):
     if path is None:
       path = self.get_default_path()
     self.path = pathlib.Path(path)
+    self.logger = logging.getLogger(__name__)
 
   def get(self, key, *, max_age=None):
     path = self.path_for_key(key)
     if not path.exists():
+      self.logger.debug("miss %s", key)
       return None
     elif max_age is not None and older_than(path, max_age):
+      self.logger.debug("miss %s", key)
       return None
     else:
+      self.logger.debug("hit %s", key)
       return path
 
   def put(self, key, content):
