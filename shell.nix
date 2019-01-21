629949607030e012f55b217955e145489a29cbf0
Kier Davis <kierdavis@gmail.com>
2019-01-21 11:07:24 +0000

    shell.nix: allow package set to be overridden
    This is required since pipenv is currently broken on the Nix stable
    channel (18.09), so must be built from the unstable channel instead.
    See also: https://github.com/NixOS/nixpkgs/issues/51970

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
-with import <nixpkgs> {};
+{ pkgs ? import <nixpkgs> {} }:
 
-stdenv.mkDerivation {
+# Note that as of 2019-01-21, pipenv is currently broken in the NixOS 18.09 release.
+# https://github.com/NixOS/nixpkgs/issues/51970
+# To work around, invoke with:
+# nix-shell -E 'import ./shell.nix { pkgs = import (fetchTarball https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz) {}; }'
+
+pkgs.stdenv.mkDerivation {
   name = "sb-bomtool-env";
-  buildInputs = [
+  buildInputs = with pkgs; [
     pipenv
     libxml2
     libxslt
   ];
-  NIX_CFLAGS_COMPILE = [ "-isystem ${libxml2.dev}/include/libxml2" ];
+  NIX_CFLAGS_COMPILE = [ "-isystem ${pkgs.libxml2.dev}/include/libxml2" ];
 }
