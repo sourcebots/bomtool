9d5f01d013dba66c96c2e0c6bc95a8f0c7671313
Kier Davis <kierdavis@gmail.com>
2019-05-27 17:19:48 +0100

    Not sure if needed but things are currently working so lets not break anything

--- shell.nix
+++ shell.nix
@@ -10,6 +10,9 @@ pkgs.stdenv.mkDerivation {
   buildInputs = with pkgs; [
     python36
     pipenv
+    libxml2
+    libxslt.dev
+    libxslt.out
   ];
   NIX_CFLAGS_COMPILE = [ "-isystem ${pkgs.libxml2.dev}/include/libxml2" ];
   LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.libxml2 pkgs.libxslt.out ];
